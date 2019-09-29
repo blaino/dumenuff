a4986cb900457005ac7b00888b7bb8c91355d4a2
Blaine Nelson <blaine.m.nelson@gmail.com>
2019-09-29 09:25:16 -0400

    A start on rules

---
+++ dumenuff_engine/lib/rules.ex
@@ -0,0 +1,22 @@
+defmodule DumenuffEngine.Rules do
+  alias __MODULE__
+
+  defstruct state: :initialized,
+    num_players: 0,
+    players_to_start: 3
+
+  def new(), do: %Rules{}
+
+  def check(%Rules{state: :initialized} = rules, :add_player) do
+    rules = Map.update!(rules, :num_players, &(&1 + 1))
+    case all_players_set?(rules) do
+      true -> {:ok, %Rules{rules | state: :players_set}}
+      false -> {:ok, rules}
+    end
+  end
+
+  def check(_state, _action), do: :error
+
+  defp all_players_set?(rules), do: rules.num_players == rules.players_to_start
+
+end

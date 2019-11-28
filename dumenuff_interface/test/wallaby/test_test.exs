18de8aa3a59557a3316e6d7e8eb6f54ef81afcaa
Blaine Nelson <blaine.m.nelson@gmail.com>
2019-11-28 05:58:17 -0500

    Initial through scores wallaby test running

--- dumenuff_interface/test/wallaby/test_test.exs
+++ dumenuff_interface/test/wallaby/test_test.exs
@@ -23,21 +23,21 @@ defmodule DumenuffInterface.UserListTest do
     assert true
   end
 
-  # try 3 players and assume 2 person game
   test "Add two players and start the game", %{session: session} do
     {:ok, player1} = Wallaby.start_session()
 
     player1
     |> visit("/")
-    |> fill_in(css("#params_name"), with: "alice")
-    |> click(button("Add"))
+    |> fill_in(css("#player_name"), with: "alice")
+    |> click(button("Enter"))
+    |> assert_has(css(".waiting", text: "Waiting"))
 
     {:ok, player2} = Wallaby.start_session()
 
     player2
     |> visit("/")
-    |> fill_in(css("#params_name"), with: "bob")
-    |> click(button("Add"))
+    |> fill_in(css("#player_name"), with: "bob")
+    |> click(button("Enter"))
 
     player1
     |> assert_has(button("DONE"))
@@ -49,15 +49,16 @@ defmodule DumenuffInterface.UserListTest do
 
     player3
     |> visit("/")
-    |> fill_in(css("#params_name"), with: "mike")
-    |> click(button("Add"))
+    |> fill_in(css("#player_name"), with: "mike")
+    |> click(button("Enter"))
+    |> assert_has(css(".waiting", text: "Waiting"))
 
     {:ok, player4} = Wallaby.start_session()
 
     player4
     |> visit("/")
-    |> fill_in(css("#params_name"), with: "zrobert")
-    |> click(button("Add"))
+    |> fill_in(css("#player_name"), with: "zrobert")
+    |> click(button("Enter"))
 
     player3
     |> assert_has(button("DONE"))
@@ -65,5 +66,14 @@ defmodule DumenuffInterface.UserListTest do
     player4
     |> assert_has(button("DONE"))
     |> take_screenshot
+
+    player1
+    |> click(button("DONE"))
+    |> assert_has(css(".waiting", text: "This game"))
+
+    player2
+    |> click(button("DONE"))
+    |> assert_has(css(".score", count: 4, text: "bonus"))
+
   end
 end
